8bf5b7f1871fd3ebbd3cb8478b39ecfa741607d8
justin <jssmith@gatech.edu>
2016-01-19 17:41:47 -0500

    added command to set necessary paths before running test

--- src/compare_matlab.m
+++ src/compare_matlab.m
@@ -1,3 +1,6 @@
+path(path,'~/Documents/simulator/');
+startup_rvc
+
 func = near_ident_dynamics_tracking_func(1,2,1,.01);
 
 func(0,[0;0;0;0;0;.5],0,0,0)
@@ -12,4 +15,3 @@ func(0,[1;2;1;0;0;.5],0,[0;1],0)
 func(0,[1;2;1;1;1;.5],0,[0;1],0)
 
 func(0,[1;2;1;1;1;.5],[5;1],[0;1],0)
-
