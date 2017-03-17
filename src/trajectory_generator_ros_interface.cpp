c0e50615ff696d2d4270ba2c74713af582a0e8ab
justin <jssmith@gatech.edu>
2017-03-17 18:36:57 -0400

    Fixed CMakeLists issue that prevented catkin build from finding Eigen

--- src/trajectory_generator_ros_interface.cpp
+++ src/trajectory_generator_ros_interface.cpp
@@ -138,7 +138,7 @@
 
 
 TrajectoryGeneratorBridge::TrajectoryGeneratorBridge() :
-trajectory_gen()  //Another option is to use a smart pointer
+trajectory_gen()
 {
     robot_radius_ = .15;  //only used to initialize lambda; maybe there is a better way (to initialize it)? Definitely should pass in robot radius if it is needed
 }
